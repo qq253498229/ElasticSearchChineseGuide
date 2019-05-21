aa341add6bb40293246ed09cc89ea26b055e040c
王斌 <253498229@qq.com>
2019-05-21 18:03:23 +0800

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-asciidoctor en/6.8.0/index.asciidoc -o ./index.html
+asciidoctor -a stylesheet=../../base/css.css -o ./index.html ./asciidoc/cn/6.8.0/index.asciidoc
+rm -rf ./images
+mkdir ./images
+cp -r asciidoc/base/images .
