a673be9ccd25383ba0aa53b8e762b4e7708a7574
王斌 <253498229@qq.com>
2019-05-23 17:45:18 +0800

    polish

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ rm -rf ./docs/images
 mkdir ./docs/images
 cp -r ./asciidoc/base/images ./docs
 
-#asciidoctor -a stylesheet=../../base/css.css -o ./docs/6.8.0/index.html ./asciidoc/cn/6.8.0/index.asciidoc
+asciidoctor -a stylesheet=../../base/css.css -o ./docs/6.8.0/index.html ./asciidoc/cn/6.8.0/index.asciidoc
 asciidoctor -a stylesheet=../../base/css.css -o ./docs/7.1.0/index.html ./asciidoc/cn/7.1.0/index.asciidoc
